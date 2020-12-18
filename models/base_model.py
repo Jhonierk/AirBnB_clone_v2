c93b0648abc204d0196b43ca661be8931d65a41c
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-12-17 21:26:29 -0500

    fix 2

--- models/base_model.py
+++ models/base_model.py
@@ -16,28 +16,26 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value_2 = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value_2)
-                elif not key == '__class__':
-                    setattr(self, key, value)
-            if self.id is None:
-                setattr(self, 'id', str(uuid.uuid4()))
-            if self.created_at is None:
-                setattr(self, 'created_at', datetime.utcnow())
-            if self.updated_at is None:
-                setattr(self, 'updated_at', datetime.utcnow())
-        else:
+         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
